6c80feb2bef17a44053bbfb8d4fa671fb4f36433
Maxime <maxime.morin2@gmail.com>
2017-05-24 18:22:14 -0400

    fix determineDates export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { AutoSizer } from 'react-virtualized'
 import WeekScheduler from './WeekScheduler'
 import Grid from './Grid'
-import * as determineDates from './utils/determine-dates';
+import determineDates from './utils/determine-dates';
 
 export {
   AutoSizer,
